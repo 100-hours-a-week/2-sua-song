6051f9888b9130a2b3447994e0b4b14bf8febbfe
gongsua <thddbswls11@naver.com>
2025-02-03 19:39:25 +0900

    homework

--- src/starbucks/Main.java
+++ src/starbucks/Main.java
@@ -2,11 +2,11 @@ package starbucks;
 
 import starbucks.item_order.Desert;
 import starbucks.item_order.Drink;
+import starbucks.item_order.Item_Order;
 import starbucks.membership_info.MemberShipGrade;
 import starbucks.membership_info.Membership;
 import starbucks.membership_info.Order;
 //import starbucks.item_order.Order;
-
 import java.util.Scanner;
 
 public class Main {
@@ -14,42 +14,22 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("🎉 스타벅스에 오신 것을 환영합니다! 🎉");
         System.out.println("==================================================");
-        System.out.println("메뉴를 골라주세요");
-        System.out.println("==================================================");
-        System.out.println("📌 음료 메뉴:");
-        //음료, 디저트 가져오기
-        Drink[] drinksarray = Drink.getDrinkMenu();
-        for (int i = 0; i < drinksarray.length; i++) {
-            System.out.println((i + 1) + "." + drinksarray[i].getName() + " " + drinksarray[i].getPrice());
-        }
-        System.out.print("\n음료를 선택하세요 (번호 입력): ");
-        int drinkChoice = in.nextInt();
-        //인덱스 1 제거해서 고른 메뉴 가져오기
-        Drink selectedDrink = drinksarray[drinkChoice - 1];
-        System.out.print("\n선택한 음료 : " + selectedDrink.getName());
 
+        Item_Order.drink_order(in);
 
         System.out.println("\n==================================================\n");
-        System.out.println("📌 디저트 메뉴 :");
 
-        Desert[] desertsarray = Desert.DessrtMenu();
-        for (int i = 0; i < desertsarray.length; i++) {
-            System.out.println((i + 1) + "." + desertsarray[i].getName() + " " + desertsarray[i].getPrice());
-        }
+        Item_Order.dessert_order(in);
 
-        System.out.println("\n 디저트를 선택하세요 (번호 입력) : ");
-        int desertChoice = in.nextInt();
-        Desert selectedDesert = desertsarray[desertChoice - 1];
-        System.out.println("\n 선택한 디저트 : " + selectedDesert.getName());
-        System.out.println("\n==================================================\n");
-        
-        
-        //음식 가격
-        int totalPrice = selectedDesert.getPrice() + selectedDrink.getPrice();
+//
 
+        Item_Order.totalPrice();
+
+        int totalPrice = Item_Order.totalPrice();
         //total 가격을 Order 전달하기
         Order order = new Order(totalPrice);
         //멤버십 넣기
+        System.out.println("==================================================");
         order.apply_order(in);
         //order부분 메서드 가져오기
         order.printfinalprice();
--- src/starbucks/item_order/Item_Order.java
+++ src/starbucks/item_order/Item_Order.java
@@ -1,4 +1,62 @@
 package starbucks.item_order;
+import java.util.Scanner;
 
 public class Item_Order {
+    private Drink drink;
+    private Desert desert;
+    private static Drink selectedDrink;
+    private static Desert selectedDesert;
+
+
+    //생성자
+
+    public Item_Order(Drink drink, Desert desert) {
+        this.drink = drink;
+        this.desert = desert;
+    }
+
+    //drink메서드
+    public static void drink_order(Scanner in) {
+        System.out.println("==================================================");
+        System.out.println("메뉴를 골라주세요");
+        System.out.println("==================================================");
+        System.out.println("📌 음료 메뉴:");
+        Drink[] drinkarray = Drink.getDrinkMenu();
+        for (int i = 0; i < drinkarray.length; i++) {
+            System.out.println((i + 1) + "." + drinkarray[i].getName() + " " + drinkarray[i].getPrice());
+        }
+
+        System.out.println("\n 음료를 선택하세요(번호 입력) : ");
+        int choice = in.nextInt();
+        selectedDrink = drinkarray[choice - 1];
+        System.out.print("\n선택한 음료 : " + selectedDrink.getName());
+
+    }
+
+
+    public static void dessert_order(Scanner in) {
+        System.out.println("📌 디저트 메뉴 :");
+        Desert[] desertsarray = Desert.DessrtMenu();
+        for (int i = 0; i < desertsarray.length; i++) {
+            System.out.println((i + 1) + "." + desertsarray[i].getName() + " " + desertsarray[i].getPrice());
+        }
+
+        System.out.println("\n 디저트를 선택하세요 (번호 입력) : ");
+        int desertChoice = in.nextInt();
+        selectedDesert = desertsarray[desertChoice - 1];
+
+        System.out.println("\n 선택한 디저트 : " + selectedDesert.getName());
+        System.out.println("\n==================================================\n");
+    }
+
+    //총 가격 메서드
+    public static int totalPrice() {
+        if(selectedDesert == null || selectedDrink == null) {
+            System.out.println("음료 또는 디저트가 선택되지 않았습니다.");
+            return 0;
+        }
+        int totalPrice = selectedDesert.getPrice() + selectedDrink.getPrice();
+        System.out.println("결제해야하는 총 금액 : " + totalPrice + "원");
+        return totalPrice;
+    }
 }
